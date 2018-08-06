757ef76fe7e9d7dbe8be7115ea14949485709bb8
TacMechMonkey <34081660+TacMechMonkey@users.noreply.github.com>
2018-08-06 20:20:45 +1000

    Update urllibLambdaWebpageCheck.py

--- urllibLambdaWebpageCheck.py
+++ urllibLambdaWebpageCheck.py
@@ -1,10 +1,10 @@
+# Original from https://github.com/TacMechMonkey/urllib-lambda-webpage-check/edit/master/urllibLambdaWebpageCheck.py
+# Known issue: Lambda/SNS produces 2 emails for failures if run in Lambda - https://stackoverflow.com/questions/51705061/lambda-boto3-python-issue
+
 # This python/boto3/lambda script sends a request to an Office 365 landing page, parses return details to confirm a successful /
 # redirect to the organisation's ADFS homepage, authenticates homepage is correct, raises any errors, and sends a consolodated /
 # report to an AWS SNS topic.
 # Run once to produce pageserver and htmlchar values for global variables.
-
-# Original from https://github.com/TacMechMonkey/urllib-lambda-webpage-check/edit/master/urllibLambdaWebpageCheck.py
-# Known issue: Lambda/SNS produces 2 emails for failures if run in Lambda - https://stackoverflow.com/questions/51705061/lambda-boto3-python-issue
 
 # Import required modules
 import boto3
